7be5d82b6b4a9de81b7c4c9a0c1e7c7677a03cf9
Enes <enstrkmn@hotmail.com>
2022-07-13 12:55:33 +0300

    step definitions and Runners

--- src/test/java/com/bluecrm/runners/CukesRunner.java
+++ src/test/java/com/bluecrm/runners/CukesRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+
         plugin = {
           "html:target/cucumber-report.html",
                 "json:target/cucumber.json"
